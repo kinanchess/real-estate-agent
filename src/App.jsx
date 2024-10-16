538b15b7afa34c601f47eeb71fcbaf9f3f43351b
KinanCCI <kenan.alaridi@gmail.com>
2024-10-16 21:30:43 +0200

    Routes for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/sign-in' element={<Signin />}></Route>
+      <Route path='/sign-up' element={<Signup />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/profile' element={<Profile />}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
