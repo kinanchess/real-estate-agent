8318926d38a455f26a04e6e643788c64cbeb8149
KinanCCI <kenan.alaridi@gmail.com>
2024-10-21 19:28:42 +0200

    built signin api route npm i jsonwebtoken

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const route = express.Router();
 
 route.post('/signup', signup);
+route.post('/signin', signin);
 
-export default signup;
+export default route;
