fcaf1c982082d7824d0f2246f6fb3f1cb9959164
KinanCCI <kenan.alaridi@gmail.com>
2024-10-24 13:54:38 +0200

    toolkit redux has been added and a user slice was created

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 
 export default function Signin() {
 
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false)
+  const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e)=> {
     setFormData(
       {
@@ -20,7 +24,8 @@ export default function Signin() {
   const handleSubmit = async(e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', 
         {
           method: 'POST',
@@ -33,18 +38,21 @@ export default function Signin() {
       const data = await res.json();
       console.log(data);
       if(data.success === false){
-        setLoading(false);
-        setError(data.message);
+        // setLoading(false);
+        // setError(data.message);
+        dispatch(signInFailure(data.message));
         
         return;
       }
-      setLoading(false)
-      setError(null)
+      // setLoading(false)
+      // setError(null)
+      dispatch(signInSuccess(data));
       navigate('/')
   
     } catch (error) {
-      setLoading(false);
-      setError(error.message)
+      // setLoading(false);
+      // setError(error.message)
+      dispatch(signInFailure(error.message))
     }
   
   };
